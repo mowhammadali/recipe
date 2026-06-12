1ccea3f863d7dfe42f439c89db581b8fdeb6143f
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-06-12 19:43:47 +0330

    [implement] navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { IconComponent } from '../../shared/components/icon/icon.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
     selector: 'app-navbar',
-    imports: [IconComponent],
+    imports: [IconComponent, RouterLink, RouterLinkActive],
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.css'],
 })
