a2ae7585aea8569c492ff22420b43a19fe880567
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-06-13 21:11:30 +0330

    [implement] truncate pipe for long text

--- src/app/components/recipe-carousel/recipe-carousel.component.ts
+++ src/app/components/recipe-carousel/recipe-carousel.component.ts
@@ -1,14 +1,15 @@
-import { Component, Input, CUSTOM_ELEMENTS_SCHEMA, input } from '@angular/core';
+import { Component, Input, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IconComponent } from '../../shared/components/icon/icon.component';
 import { NgFor } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { type RecipeType } from '../../types/recipes.type';
+import { TruncatePipe } from '../../pipes/truncate.pipe';
 
 @Component({
     selector: 'app-recipe-carousel',
     standalone: true,
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    imports: [IconComponent, NgFor, RouterLink],
+    imports: [IconComponent, NgFor, RouterLink, TruncatePipe],
     templateUrl: './recipe-carousel.component.html',
     styleUrl: './recipe-carousel.component.css',
 })
