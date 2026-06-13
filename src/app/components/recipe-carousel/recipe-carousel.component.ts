424cd213d1dd13738de00195f7d12d1b18319811
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-06-13 18:16:38 +0330

    [implement] carousels

--- src/app/components/recipe-carousel/recipe-carousel.component.ts
+++ src/app/components/recipe-carousel/recipe-carousel.component.ts
@@ -1,12 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IconComponent } from '../../shared/components/icon/icon.component';
+import { NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-recipe-carousel',
-    imports: [IconComponent],
+    standalone: true,
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    imports: [IconComponent, NgFor, RouterLink],
     templateUrl: './recipe-carousel.component.html',
     styleUrl: './recipe-carousel.component.css',
 })
 export class RecipeCarouselComponent {
     @Input('carousel-title') title: string = '';
+
+    recipes = [
+        { title: 'Mixin sea food', time: 6, image: '/images/1.webp' },
+        { title: 'Chicken burger', time: 10, image: '/images/1.webp' },
+        { title: 'Pasta cream', time: 8, image: '/images/1.webp' },
+        { title: 'Beef steak', time: 15, image: '/images/1.webp' },
+        { title: 'Veg salad', time: 5, image: '/images/1.webp' },
+    ];
 }
