a18a08c4caf8b893419f305dd540450559f0e9db
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-06-15 13:35:03 +0330

    [implement] navigation to recipe page

--- src/app/components/recipe-card/recipe-card.component.ts
+++ src/app/components/recipe-card/recipe-card.component.ts
@@ -2,13 +2,23 @@ import { Component, Input } from '@angular/core';
 import { IconComponent } from '../../shared/components/icon/icon.component';
 import { TruncatePipe } from '../../pipes/truncate.pipe';
 import { type RecipeType } from '../../types/recipes.type';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-recipe-card',
-    imports: [IconComponent, TruncatePipe],
+    imports: [IconComponent, TruncatePipe, RouterLink],
     templateUrl: './recipe-card.component.html',
     styleUrl: './recipe-card.component.css',
 })
 export class RecipeCardComponent {
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute
+    ) {}
+
     @Input() recipe = {} as RecipeType;
+
+    public navigateToRecipeDetails(id: number): void {
+        this.router.navigate(['.', id], { relativeTo: this.route });
+    }
 }
