3a78619931c2f2a8784ba265e5e7f8951f64e7b8
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-06-16 15:52:50 +0330

    [implement] auth interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { register } from 'swiper/element/bundle';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { AuthInterceptor } from './core/auth.interceptor';
 
 register();
 
@@ -12,8 +13,13 @@ export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
-        provideHttpClient(),
         provideAnimations(),
         provideToastr(),
+        provideHttpClient(withInterceptorsFromDi()),
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptor,
+            multi: true,
+        },
     ],
 };
