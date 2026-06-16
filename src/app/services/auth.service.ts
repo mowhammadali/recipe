e9eb3404bd60e6c7fd49e6e71e7ee07ff00ee6b1
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-06-16 15:42:45 +0330

    [add] auth service methods and properties

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../core/dummy-api.service';
-import type { UserDataType, LoginResponseType } from '../types/auth.type';
+import type { UserDataType, LoginResponseType, AuthType } from '../types/auth.type';
+import { BehaviorSubject, catchError, map, Observable, of, tap } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
@@ -8,11 +9,62 @@ import type { UserDataType, LoginResponseType } from '../types/auth.type';
 export class AuthService {
     constructor(private apiService: ApiService) {}
 
-    public loginUser(userData: UserDataType) {
-        return this.apiService.post<LoginResponseType>('/auth/login', userData, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+    private userSubject = new BehaviorSubject<AuthType | null>(null);
+
+    user$ = this.userSubject.asObservable();
+
+    isAuthenticated$ = this.user$.pipe(map((user) => !!user));
+
+    private tokenKey = 'accessToke';
+
+    get token(): string | null {
+        return localStorage.getItem(this.tokenKey);
+    }
+
+    private setToken(token: string) {
+        localStorage.setItem(this.tokenKey, token);
+    }
+
+    private removeToken() {
+        localStorage.removeItem(this.tokenKey);
+    }
+
+    public loginUser(userData: UserDataType): Observable<LoginResponseType> {
+        return this.apiService
+            .post<LoginResponseType>('/auth/login', userData, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            .pipe(
+                tap((response) => {
+                    this.setToken(response.accessToken);
+                    this.userSubject.next(response);
+                })
+            );
+    }
+
+    public getUserInfo(): Observable<AuthType | null> {
+        if (!this.token) {
+            return of(null);
+        }
+
+        return this.apiService.get<AuthType>('/auth/me').pipe(
+            tap((response) => {
+                this.userSubject.next(response);
+            }),
+            catchError(() => {
+                return of(null);
+            })
+        );
+    }
+
+    initAuth(): Observable<boolean> {
+        return this.getUserInfo().pipe(map((user) => !!user));
+    }
+
+    logout() {
+        this.removeToken();
+        this.userSubject.next(null);
     }
 }
