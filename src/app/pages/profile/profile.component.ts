1ad8a99013c5e3e7e2557105a4c62d390a2a9c3f
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-06-17 10:53:41 +0330

    [implement] profile UI

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,9 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { type AuthType } from '../../types/auth.type';
+import { map, tap } from 'rxjs';
+import { Router } from '@angular/router';
+import { IconComponent } from '../../shared/components/icon/icon.component';
 
 @Component({
     selector: 'app-profile',
-    imports: [],
+    imports: [IconComponent],
     templateUrl: './profile.component.html',
     styleUrl: './profile.component.css',
 })
-export class ProfileComponent {}
+export class ProfileComponent implements OnInit {
+    constructor(
+        private authService: AuthService,
+        private router: Router
+    ) {}
+
+    public userInfo = signal({} as AuthType);
+
+    public ngOnInit(): void {
+        this.getUserInfo();
+    }
+
+    public getUserInfo() {
+        this.authService
+            .getUserInfo()
+            .pipe(
+                map((response) => {
+                    if (response == null) {
+                        return {} as AuthType;
+                    }
+                    return response;
+                }),
+                tap((response) => {
+                    if (response == null) {
+                        this.authService.logout();
+                        this.router.navigate(['login']);
+                        return;
+                    }
+                })
+            )
+            .subscribe({
+                next: (response) => {
+                    console.log(response);
+
+                    this.userInfo.set(response);
+                },
+            });
+    }
+}
