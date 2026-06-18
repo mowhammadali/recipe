68738ffaefa3a456c6dee4c4f2416a820d3665d0
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-06-18 14:23:03 +0330

    [implement] create recipe UI

--- src/app/pages/create-recipe/create-recipe.component.ts
+++ src/app/pages/create-recipe/create-recipe.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { AuthControllerComponent } from '../../components/auth-controller/auth-controller.component';
+import { IconComponent } from '../../shared/components/icon/icon.component';
+import { ButtonComponent } from '../../shared/components/button/button.component';
 
 @Component({
     selector: 'app-create-recipe',
-    imports: [AuthControllerComponent],
+    imports: [AuthControllerComponent, IconComponent, ButtonComponent],
     templateUrl: './create-recipe.component.html',
     styleUrl: './create-recipe.component.css',
 })
